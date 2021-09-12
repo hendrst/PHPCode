c1c3dc2b7443519454f1e7cabb7b49e3d374d339
hendrst <steve@stevehendren.com>
2021-09-11 17:03:06 -0700

    Update

--- globe_bank/private/db_credentials.php
+++ globe_bank/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
     define ("DB_SERVER", "localhost");
     define ("DB_USER", "webuser");
-    define ("DB_PWD", "51stncst!");
+    define ("DB_PWD", "********");
     define ("DB_NAME", "globe_bank");
 ?>
